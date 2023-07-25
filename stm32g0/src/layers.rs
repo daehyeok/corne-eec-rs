468e6a72d4a6463e006c3cd986b5221a6990c8db
Daehyeok Mun <daehyeok@gmail.com>
2023-07-24 23:55:32 -0700

    Change layout

--- stm32g0/src/layers.rs
+++ stm32g0/src/layers.rs
@@ -3,7 +3,7 @@ use embassy_sync::blocking_mutex::{raw::ThreadModeRawMutex, Mutex};
 use keyberon::{
     action::{
         k, l,
-        Action::{self, HoldTap},
+        Action::{self, HoldTap, Trans},
         HoldTapAction, HoldTapConfig,
     },
     key_code::KeyCode::*,
@@ -24,12 +24,14 @@ pub fn new_shared_layout() -> SharedLayout {
 }
 
 const FNSPC: Action = HoldTap(&HoldTapAction {
-    timeout: 200,
+    timeout: 50,
     tap_hold_interval: 0,
     config: HoldTapConfig::HoldOnOtherKeyPress,
     hold: l(1),
     tap: k(Space),
 });
+
+const _____: Action = Trans;
 
 #[rustfmt::skip]
 pub static LAYERS: Layers  = layout::layout! {
@@ -39,13 +41,13 @@ pub static LAYERS: Layers  = layout::layout! {
 /*Row1*/[Tab     Q        W        E        R        T        Y        U        I        O        P        Bslash  ]
 /*Row2*/[LCtrl   A        S        D        F        G        H        J        K        L        SColon   Quote   ]
 /*Row3*/[LShift  Z        X        C        V        B        N        M        Comma    Dot      Slash    RShift  ]
-/*Row4*/[No      No       No       LGui     LAlt     {FNSPC}  {FNSPC}  Enter    Down     Up       No       No     ]
+/*Row4*/[No      No       No       No       LAlt     {FNSPC}  {FNSPC}  Enter    Down     Up       No       No      ]
     }{
 //     | 00(L0) | 01(L1) | 02(L2) | 03(L3) | 04(L4) | 05(L5) | 06(R0) | 07(R1) | 08(R2) | 09(R3) | 10(R4) | 11(R5) |
-/*Row0*/[Escape  No       No       No       No       No       No       No       No       Minus    Equal    No      ]
-/*Row1*/[No      No       No       No       No       No       No       No       No       LBracket RBracket No      ]
-/*Row2*/[No      No       No       No       No       No       No       Left     Down     Up       Right    No      ]
-/*Row3*/[No      No       No       No       No       No       No       No       No       No       No       No      ]
-/*Row4*/[No      No       No       No       No       No       No       No       No       No       No       No      ]
+/*Row0*/[Escape  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  Minus    Equal    {_____} ]
+/*Row1*/[{_____} {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  LBracket RBracket {_____} ]
+/*Row2*/[{_____} {_____}  {_____}  {_____}  {_____}  {_____}  Left     Down     Up       Right    {_____}  {_____} ]
+/*Row3*/[{_____} {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____}  {_____} ]
+/*Row4*/[{_____} {_____}  {_____}  {_____}  {_____}  Space    Space    {_____}  {_____}  {_____}  {_____}  {_____} ]
     }
 };
